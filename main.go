e07c47b630c04362832eba4b51247dc75db8e0ef
leszekbulawa <leszekbu@gmail.com>
2019-04-17 13:38:52 +0100

    Use goquery to print all image URLs

--- main.go
+++ main.go
@@ -1,13 +1,11 @@
 package main
 
 import (
-	"io/ioutil"
 	"fmt"
 	"log"
 	"net/http"
-	"os"
-	"regexp"
-	"strings"
+
+	"github.com/PuerkitoBio/goquery"
 )
 
 func main() {
@@ -28,40 +26,19 @@ func main() {
 	}
 	defer response.Body.Close()
 
-	// Get response body as string
-	dataInBytes, err := ioutil.ReadAll(response.Body)
-	pageContent := string(dataInBytes)
-
-	// Find title substring
-	titleStartIndex := strings.Index(pageContent, "<title>")
-	if titleStartIndex == -1 {
-		fmt.Println("No title element found")
-		os.Exit(0)
-	}
-	// Title offset to exclude <title>
-	titleStartIndex += 7
-
-	// Find index of title closing tag
-	titleEndIndex := strings.Index(pageContent, "</title>")
-	if titleEndIndex == -1 {
-		fmt.Println("No title closing found")
-		os.Exit(0)
+	// Create a goquery document from the HTTP response
+	document, err := goquery.NewDocumentFromReader(response.Body)
+	if err != nil {
+		log.Fatal("Error loading HTTP response body. ", err)
 	}
 
-	// Copy title do separate variable to let GC wipe whole doc
-	pageTitle := []byte(pageContent[titleStartIndex:titleEndIndex])
-
-	// Print result
-	fmt.Printf("Page title: %s\n", pageTitle)
-
-	// Regex to find image urls
-	re := regexp.MustCompile("<img src.*>")
-	imageUrls := re.FindAllString(pageContent, -1)
-	if imageUrls == nil {
-		fmt.Println("No matches.")
-	} else {
-		for _, imageUrl := range imageUrls {
-			fmt.Println(imageUrl)
+	// Find and print image URLs
+	document.Find("img").Each(func(index int, element *goquery.Selection) {
+		imgSrc, exists := element.Attr("src")
+		if exists {
+			fmt.Println(imgSrc)
 		}
-	}
+	})
+
+
 }
