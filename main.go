2cfcf4e217330102c8010c3632c0d7a8e018bdd0
leszekbulawa <leszekbu@gmail.com>
2019-05-01 20:40:57 +0200

    Code refactor

--- main.go
+++ main.go
@@ -16,22 +16,12 @@ func main() {
 	// Create http client
 	client := &http.Client{}
 
-	// Create custom http request
-	request, err := http.NewRequest("GET", "https://jbzd.pl", nil)
-	if err != nil {
-		log.Fatal(err)
-	}
-	request.Header.Set("User-Agent", "Jbzd Imgcat - https://github.com/leszekbulawa/go_jbzd_imgcat")
-
-	// Make http GET request
-	response, err := client.Do(request)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer response.Body.Close()
+	// Get jbzd main page
+	main_page := imgcat_get_request("https://jbzd.pl", client)
+	defer main_page.Body.Close()
 
 	// Create a goquery document from the HTTP response
-	document, err := goquery.NewDocumentFromReader(response.Body)
+	document, err := goquery.NewDocumentFromReader(main_page.Body)
 	if err != nil {
 		log.Fatal("Error loading HTTP response body. ", err)
 	}
@@ -45,30 +35,40 @@ func main() {
 			imageUrls = append(imageUrls, imgSrc)
 		}
 	})
-	// Draw random image
+
+	// Seed for random int generator
 	rand.Seed(time.Now().UnixNano())
-	fmt.Println(imageUrls[rand.Intn(len(imageUrls))])
+
+	// Get image
+	image := imgcat_get_request(imageUrls[rand.Intn(len(imageUrls))], client)
+	defer image.Body.Close()
 
 	// Get image content
-	image_request, err := http.NewRequest("GET", imageUrls[rand.Intn(len(imageUrls))], nil)
+	image_data, err := ioutil.ReadAll(image.Body)
+	if err != nil{
+		log.Fatal(err)
+	}
+
+	// Convert image to base64
+	image_b64_data := base64.StdEncoding.EncodeToString(image_data)
+
+	// Print image to terminal with magic header
+	fmt.Printf("\033]1337;File=;size=%d;inline=1:%s%s", len(image_b64_data), image_b64_data, "\a\n")
+
+}
+
+func imgcat_get_request(url string, client *http.Client) *http.Response {
+	// Create custom http request
+	request, err := http.NewRequest("GET", url, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
 	request.Header.Set("User-Agent", "Jbzd Imgcat - https://github.com/leszekbulawa/go_jbzd_imgcat")
 
-	resp, err := client.Do(image_request)
+	// Make http GET request
+	response, err := client.Do(request)
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer resp.Body.Close()
-	image_data, err := ioutil.ReadAll(resp.Body)
-	if err != nil{
-		log.Fatal(err)
-	}
-
-	image_b64_data := base64.StdEncoding.EncodeToString(image_data)
-	fmt.Println(len(image_b64_data))
-
-	// Print image to terminal
-	fmt.Printf("\033]1337;File=;size=%d;inline=1:%s%s", len(image_b64_data), image_b64_data, "\a\n")
+	return response
 }
